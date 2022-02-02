cfb72456ea955951cb0971760525874c5b062df9
houcks <houcks08@gmail.com>
2022-02-02 14:31:54 -0700

    created black and red checkerboard of 60 squares

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+document.addEventListener('DOMContentLoaded', function () {
+  let screenDiv = document.createElement('div')
+  screenDiv.style.cssText = 'height:100%;width100%';
+  document.body.appendChild(screenDiv);
+    for(var i = 0; i < 100; i++){
+      let colorDiv = document.createElement('div');
+      if(i % 2 === 1){
+        colorDiv.style.cssText = 'width:11.1%;float:left;padding-bottom:11.1%;background-color: red';
+      }
+      else{
+        colorDiv.style.cssText = 'width:11.1%;float:left;padding-bottom:11.1%;background-color: black';
+      }
+      
+      screenDiv.appendChild(colorDiv);
+    }
+  })
